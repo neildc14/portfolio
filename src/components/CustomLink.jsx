d41ee3257112f43c1587309977ee2d9c5668fa61
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-01-21 15:18:08 +0800

    added drawer for sliding mobile navbar

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link, useColorMode } from "@chakra-ui/react";
 
-export const CustomNavLink = ({ link }) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+export const CustomNavLink = ({ link, px = 2, py = 0 }) => {
+  const { colorMode } = useColorMode();
   return (
     <Link
-      px={2}
+      width="100%"
+      px={px}
+      py={py}
       borderRadius="md"
       _hover={{
         bgColor: colorMode === "dark" ? "teal.400" : "teal.200",
         color: colorMode === "dark" ? "gray.300" : "blackAlpha.800",
       }}
+      textAlign="center"
     >
       {link}
     </Link>
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { Box, IconButton, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  IconButton,
+  useColorMode,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { DarkModeIcon, LightModeIcon, HamburgerIcon } from "../utils/SVGIcons";
+import { CustomNavLink } from "./CustomLink";
 
 const MobileNav = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
 
   return (
     <Box>
@@ -18,7 +32,42 @@ const MobileNav = () => {
         variant="ghost"
         onClick={toggleColorMode}
       />
-      <IconButton icon={<HamburgerIcon boxSize={6} />} variant="ghost" />
+      <IconButton
+        icon={<HamburgerIcon boxSize={6} />}
+        variant="ghost"
+        onClick={onOpen}
+      />
+
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="full"
+      >
+        <DrawerOverlay />
+        <DrawerContent bgColor={colorMode === "light" && "blue.100"}>
+          <DrawerCloseButton />
+          <DrawerHeader>@neildelacruz</DrawerHeader>
+
+          <DrawerBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            gap={10}
+            fontSize="xl"
+            fontWeight="semibold"
+            width="100%"
+          >
+            <CustomNavLink link="Profile" py={2} />
+            <CustomNavLink link="Skills" py={2} />
+            <CustomNavLink link="Projects" py={2} />
+            <CustomNavLink link="Education" py={2} />
+            <CustomNavLink link="Contact" py={2} />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 };
