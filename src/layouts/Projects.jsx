be7589a425e182d9c5b92d3f6f3a0270eefab6ba
Neil Edward Dela Cruz <63070686+neildc14@users.noreply.github.com>
2023-01-26 12:15:53 +0800

    added new project to showcase

--- src/layouts/Projects.jsx
+++ src/layouts/Projects.jsx
@@ -7,6 +7,7 @@ import shrinky from "../assets/images/shrinky.png";
 import isquizzy from "../assets/images/isquizzy.png";
 import recifree from "../assets/images/recifree1.png";
 import mailbutler from "../assets/images/mailbutler1.png";
+import inventory from "../assets/images/inventory.png";
 
 import ProjectCard from "../components/ProjectCard";
 
@@ -30,9 +31,30 @@ const Projects = () => {
             image={shrinky}
             heading="Shrinky"
             explanation="This web app offers a simple solution to shorten long and complex URLs, making them more manageable and shareable."
-            tools={["MongoDB", "Express", "React", "NodeJS", "Charka UI"]}
+            tools={[
+              "MongoDB",
+              "Express",
+              "React",
+              "NodeJS",
+              "REST API",
+              "Chakra UI",
+            ]}
             link="https://shrinky.netlify.app"
             github="https://github.com/neildc14/shrinky"
+          />
+        </div>
+        <div
+          data-aos="fade-up"
+          data-aos-duration={500}
+          data-aos-anchor-placement="top-bottom"
+        >
+          <ProjectCard
+            image={inventory}
+            heading="Inventory_System"
+            explanation="It is a web app used to manage sales and inventory. It enables the ability to perform CRUD operations for monitoring and managing sales and orders"
+            tools={["NodeJS", "Express", "EJS", "Bootstrap", "REST API"]}
+            link="https://precious-shop.onrender.com"
+            github="https://github.com/neildc14/Product-Sales-Inventory"
           />
         </div>
         <div
@@ -58,7 +80,13 @@ const Projects = () => {
             image={recifree}
             heading="recifree"
             explanation="This project utilizes the EDAMAM Recipe Search API to provide access to millions of web recipes."
-            tools={["React", "recipe search API", "axios", "tailwind CSS"]}
+            tools={[
+              "React",
+              "recipe search API",
+              "axios",
+              "react-query",
+              "tailwind CSS",
+            ]}
             link="https://recifree.netlify.app/"
             github="https://github.com/neildc14/recifree"
           />
@@ -71,8 +99,8 @@ const Projects = () => {
           <ProjectCard
             image={mailbutler}
             heading="Mailbutler"
-            explanation="Figma design converted to static website"
-            tools={["React", "Chakra UI"]}
+            explanation="It is a Figma design for mailing website converted to static website"
+            tools={["React", "Chakra UI", "react-router-dom"]}
             link="https://mailbutler.netlify.app/"
             github="https://github.com/neildc14/mailbutler"
           />
